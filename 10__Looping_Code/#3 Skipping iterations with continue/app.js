4ef4303f427c268b97b6cd33b00792a38fbd5329
Anesti Gjikoka <anesti.gjikoka@gmail.com>
2019-02-11 21:09:59 +0100

    Update empty

---
+++ 10__Looping_Code/#3 Skipping iterations with continue/app.js
@@ -0,0 +1,15 @@
+/*
+    loop(food = 0; foodNeeded = 10) {
+    if (food >= foodNeeded) {
+        exit loop;
+        // We have enough food; let's go home
+    } else {
+        food += 2; // Spend an hour collecting 2 more food
+        // loop will then run again
+    }
+    }
+*/
+
+
+// Exiting loops with break
+
